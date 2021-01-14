d6354b5c1c5c53246f06420a1679b8a7edbc58a7
BrennanKerr <BrennanKerr@users.noreply.github.com>
2021-01-14 15:35:14 -0500

    Plugboard Class
    Added 2 new classes
    
    Plugboard - Handels the plugboard settings
    
    InvalidPlugboardException - Handles any plugboard errors
    
    Plugboard not implemented in Maching.cs as of current commit.

---
+++ EncryptionMachine/InvalidPlugboardException.cs
@@ -0,0 +1,123 @@
+﻿/*
+ * @author:     Brennan Kerr
+ * @since:      01/14/21
+ * @name:       InvalidPlugboardException.cs
+ * @desc:       Creates the InvalidPlugboardException class
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptionMachine
+{
+    /// <summary>
+    /// InvalidPlugboard - Used to handle any errors with the plugboard
+    /// </summary>
+    class InvalidPlugboardException : Exception
+    {
+        #region FIELDS
+
+        /// <summary>
+        /// The combination string
+        /// </summary>
+        private string combinations = "";
+        /// <summary>
+        /// the list that stores the error chars
+        /// </summary>
+        private List<char> errors = new List<char>();
+        /// <summary>
+        /// the reason for the errors
+        /// </summary>
+        private PLUGBOARD_ERRORS reason;
+
+        /// <summary>
+        /// Enum that stores the error list
+        /// </summary>
+        public enum PLUGBOARD_ERRORS
+        {
+            DublicatedLetters,
+            CharsNotPaired,
+            PairDoesNotExist
+        }
+
+        #endregion
+
+        #region CONSTRUCTOR
+        
+        /// <summary>
+        /// Initializes a new exception object
+        /// </summary>
+        /// <param name="combinations">the combination string</param>
+        /// <param name="errors">the error list</param>
+        /// <param name="reason">the reason for the exception</param>
+        public InvalidPlugboardException(string combinations, List<char> errors, PLUGBOARD_ERRORS reason)
+        {
+            this.combinations = combinations;
+            this.errors = errors;
+            this.reason = reason;
+        }
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Retrieves the combinations that are set
+        /// </summary>
+        public string Combinations
+        {
+            get { return Combinations; }
+        }
+
+        /// <summary>
+        /// Gets the error list
+        /// </summary>
+        public List<char> Errors
+        {
+            get { return errors; }
+        }
+
+        /// <summary>
+        /// Gets the reason for the exception
+        /// </summary>
+        public PLUGBOARD_ERRORS Reason
+        {
+            get { return reason; }
+        }
+        #endregion
+
+        #region OVERRIDES
+
+        /// <summary>
+        /// Displays the error message
+        /// </summary>
+        public override string Message
+        { 
+            get
+            {
+                string str = "PLUGBOARD ERRORS: ";
+                str += "\n" + combinations + "\n";
+
+                if (reason == PLUGBOARD_ERRORS.DublicatedLetters)
+                {
+                    str += "Dublicates: " + BasicFunctions.RunThroughList(errors);
+                }
+                else if (reason == PLUGBOARD_ERRORS.CharsNotPaired)
+                {
+                    str += "NOT PAIRED: " + BasicFunctions.RunThroughList(errors);
+                }
+                else if (reason == PLUGBOARD_ERRORS.PairDoesNotExist)
+                {
+                    str += "PAIR DOES NOT EXIST FOR " + BasicFunctions.RunThroughList(errors);
+                }
+
+                str += "\nThe new pairs will not be set";
+
+                return str;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ EncryptionMachine/Plugboard.cs
@@ -0,0 +1,176 @@
+﻿/*
+ * @author:     Brennan Kerr
+ * @since:      01/14/21
+ * @name:       Plugboard.cs
+ * @desc:       Creates the plugboard class
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptionMachine
+{
+    /// <summary>
+    /// The plugboard class is used to create an instance of plugboard
+    /// which is used to change the letter
+    /// </summary>
+    public class Plugboard
+    {
+        /// <summary>
+        /// Stores the combination string
+        /// </summary>
+        private string plugboardCombinations = "";
+
+        /// <summary>
+        /// Creates a new plugboard instance
+        /// </summary>
+        /// <param name="combinations">the combination string (Each pair is an odd and even index)</param>
+        public Plugboard(string combinations = "")
+        {
+            Combinations = combinations;
+        }
+
+        /// <summary>
+        /// Sets the combination string
+        /// </summary>
+        public string Combinations
+        {
+            get { return plugboardCombinations; }
+            set
+            {
+                // if the length is an even number
+                if (value.Length % 2 == 0)
+                {
+                    // check and store any dublicates
+                    List<char> dublicates = BasicFunctions.DuplicatedCharacters(value);
+
+                    // if there are dublicates, throw an exception
+                    if (dublicates.Count > 0)
+                        throw new InvalidPlugboardException(value, dublicates, 
+                            InvalidPlugboardException.PLUGBOARD_ERRORS.DublicatedLetters);
+                    // otherwise set the plugboard
+                    else
+                        plugboardCombinations = value;
+                }
+                // if the length is odd
+                else
+                    throw new InvalidPlugboardException(value, new List<char>() { value[value.Length - 1] }, 
+                        InvalidPlugboardException.PLUGBOARD_ERRORS.CharsNotPaired);
+            }
+        }
+
+        #region METHODS
+
+        /// <summary>
+        /// Gets the paired letter
+        /// </summary>
+        /// <param name="letter">the letter to check</param>
+        /// <returns>if the letter is in the plugboard, the paired letter, otherwise returns the same letter</returns>
+        public char GetPairedLetter(char letter)
+        {
+            // if the letter is in the plugboard list
+            if (IsPaired(letter))
+            {
+                // gets the index
+                int index = plugboardCombinations.IndexOf(letter);
+
+                // returns the paired index
+                    // (index % 2 == 0) determines if the index is even or odd
+                return plugboardCombinations[PairedLetterIndex(index)];
+            }
+            // otherwise return the same letter
+            else return letter;
+        }
+
+        /// <summary>
+        /// Checks if the letter is already paired
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <returns></returns>
+        public bool IsPaired(char letter)
+        {
+            if (plugboardCombinations.Contains(letter.ToString())) return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Sets a pair of letters
+        /// </summary>
+        /// <param name="letter1">the first letter to pair</param>
+        /// <param name="letter2">the second letter to pair</param>
+        public void SetPair(char letter1, char letter2)
+        {
+            SetPair(letter1.ToString() + letter2.ToString());
+        }
+
+        /// <summary>
+        /// Sets pairs based on a string
+        /// </summary>
+        /// <param name="letters">the string that contains the pairings</param>
+        public void SetPair(string letters)
+        {
+            // if there are an even number of pairs
+            if (letters.Length % 2 == 0)
+            {
+                // stores any dublicates
+                List<char> dublicates = new List<char>();
+
+                // checks for any dublicates
+                for (int i = 0; i < letters.Length; i++)
+                {
+                    char c = letters[i];
+                    if (IsPaired(c)) dublicates.Add(c);
+                    // if the letters are the same
+                    else if (i % 2 == 0)
+                    {
+                        if (letters[i] == letters[i + 1])
+                            dublicates.Add(c);
+                    }
+                }
+
+                // if there are dublicates
+                if (dublicates.Count > 0)
+                    throw new InvalidPlugboardException(Combinations + letters, dublicates,
+                        InvalidPlugboardException.PLUGBOARD_ERRORS.DublicatedLetters);
+                // otherwise, add the combinations to the string
+                else plugboardCombinations += letters;
+            }
+            else
+                throw new InvalidPlugboardException(Combinations + letters, new List<char>() { letters[letters.Length - 1] },
+                    InvalidPlugboardException.PLUGBOARD_ERRORS.CharsNotPaired);
+        }
+
+        /// <summary>
+        /// Removes the pair based on the letter
+        /// </summary>
+        /// <param name="letter">the letter of the pair to remove</param>
+        public void RemovePair(char letter)
+        {
+            if (!IsPaired(letter))
+                throw new InvalidPlugboardException(Combinations, new List<char>() { letter },
+                    InvalidPlugboardException.PLUGBOARD_ERRORS.PairDoesNotExist);
+            else
+            {
+                // gets the two indexes
+                int index1 = plugboardCombinations.IndexOf(letter);
+                int index2 = PairedLetterIndex(index1);
+
+                // removes the letters
+                plugboardCombinations = plugboardCombinations.Remove((index1 < index2 ? index1 : index2), 2);
+            }
+        }
+
+        /// <summary>
+        /// Gets the index of the paired letter
+        /// </summary>
+        /// <param name="i">the index of the first letter</param>
+        /// <returns>the paired letter index</returns>
+        private int PairedLetterIndex(int i)
+        {
+            return i + (i % 2 == 0 ? 1 : -1);
+        }
+
+        #endregion
+    }
+}
